d2568499bda5c92d03530d57974cacfa95e67406
Flavio Chaffin <flavio.chaffin@gmail.com>
2017-08-13 22:13:57 -0300

    MAnutencao das views

--- Projeto01/Contexts/EFContext.cs
+++ Projeto01/Contexts/EFContext.cs
@@ -1,14 +1,9 @@
 ﻿using Projeto01.Models;
-using System;
-using System.Collections.Generic;
 using System.Data.Entity;
-using System.Linq;
-using System.Web;
-using MySql.Data.Entity;
 
 namespace Projeto01.Contexts
 {
-	public class EFContext : DbContext
+    public class EFContext : DbContext
 	//public class EFContext : DbContext
 	{
 		//public EFContext() : base("Asp_Net_MVC_CS") {
